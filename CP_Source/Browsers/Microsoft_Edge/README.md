ec3be947f2b2736c053ba7906ee01c56637b263e
neher-igel <neher@igel.com>
2021-03-05 14:19:06 -0600

    Microsoft Edge (with build tar script)

---
+++ CP_Source/Browsers/Microsoft_Edge/README.md
@@ -0,0 +1,16 @@
+# Microsoft Edge (DEV Channel)
+
+|  CP Information |            |
+|-----------------|------------|
+| Package | [Microsoft Edge](https://www.microsoftedgeinsider.com/en-us/download?platform=linux-deb) - Current Stable Version |
+| Script Name | [edge-cp-init-script.sh](edge-cp-init-script.sh) |
+| CP Mount Path | /custom/edge |
+| CP Size | 600M |
+| IGEL OS Version (min) | 11.4.240 |
+| Metadata File <br /> edge.inf | [INFO] <br /> [PART] <br /> file="edge.tar.bz2" <br /> version="90.0.810.1" <br /> size="600M" <br /> name="edge" <br /> minfw="11.04.240" |
+| Path to Executable | /custom/edge/usr/bin/microsoft-edge-dev |
+| Path to Icon | /custom/edge/opt/microsoft/msedge-dev/product_logo_256_dev.png |
+| Missing Libraries | None |
+| Download package and missing library | apt-get download microsoft-edge-dev <br /> apt-get download libatomic1 |
+| Packaging Notes | Create folder: **edge** <br /><br /> dpkg -x <package/lib> custom/edge <br /><br /> Need to move the mime folder: <br /><br />mv /custom/edge/usr/share/applications /custom/edge/usr/share/applications.mime <br /><br />The init script needs additional files to configure AppArmor: <br /><br /> /custom/edge/config/bin/[edge_cp_apparmor_reload](edge_cp_apparmor_reload) <br /> /custom/edge/lib/systemd/system/[igel-edge-cp-apparmor-reload.service](igel-edge-cp-apparmor-reload.service) |
+| Package automation | [build-edge-cp.sh](build-edge-cp.sh) <br /><br /> Tested with 90.0.810.1
---
+++ CP_Source/Browsers/Microsoft_Edge/edge-cp-init-script.sh
@@ -0,0 +1,68 @@
+#! /bin/bash
+#set -x
+#trap read debug
+
+ACTION="custompart-edge_${1}"
+
+# mount point path
+MP=$(get custom_partition.mountpoint)
+
+# custom partition path
+CP="${MP}/edge"
+
+# user directories
+EDGE_USER_CONFIG="/userhome/.config/microsoft-edge-dev"
+
+# output to systemlog with ID amd tag
+LOGGER="logger -it ${ACTION}"
+
+echo "Starting" | $LOGGER
+
+case "$1" in
+init)
+  # Linking files and folders on proper path
+  find ${CP} | while read LINE
+  do
+    DEST=$(echo -n "${LINE}" | sed -e "s|${CP}||g")
+    if [ ! -z "${DEST}" -a ! -e "${DEST}" ]; then
+      # Remove the last slash, if it is a dir
+      [ -d $LINE ] && DEST=$(echo "${DEST}" | sed -e "s/\/$//g") | $LOGGER
+      if [ ! -z "${DEST}" ]; then
+        ln -sv "${LINE}" "${DEST}" | $LOGGER
+      fi
+    fi
+  done
+
+  # fix permissions
+  chmod 4755 "$CP/opt/microsoft/msedge-dev/msedge-sandbox"
+
+  # basic persistency
+  chown -R user:users "${CP}${EDGE_USER_CONFIG}"
+
+  # Add apparmor profile to trust in Firefox to make SSO possible
+  # We do this by a systemd service to run the reconfiguration
+  # surely after apparmor.service!!!
+  systemctl --no-block start igel-edge-cp-apparmor-reload.service
+
+  # after CP installation run wm_postsetup to activate mimetypes for SSO
+  if [ -d /run/user/777 ]; then
+    wm_postsetup
+    # delay the CP ready notification
+    sleep 3
+  fi
+
+;;
+stop)
+  # unlink linked files
+  find ${CP} | while read LINE
+  do
+    DEST=$(echo -n "${LINE}" | sed -e "s|${CP}||g")
+    unlink $DEST | $LOGGER
+  done
+
+;;
+esac
+
+echo "Finished" | $LOGGER
+
+exit 0
