3af1bdd33bfb3b3cb40a854bc6a18a410043cfe1
neher-igel <neher@igel.com>
2021-06-24 12:45:38 -0500

    Microsoft .NET 5.0 Runtime (24 June)

---
+++ CP_Source/Apps/Microsoft_NET_Runtime/README.md
@@ -0,0 +1,12 @@
+# Microsoft .NET 5.0 Runtime (24 June)
+
+|  CP Information |            |
+|-----------------|------------|
+| Package | [Microsoft .NET 5.0 Runtime](https://docs.microsoft.com/en-us/dotnet/core/install/linux-ubuntu)
+| Script Name | [dotnet-cp-init-script.sh](dotnet-cp-init-script.sh) |
+| CP Mount Path | /custom/dotnet |
+| CP Size | 200M |
+| IGEL OS Version (min) | 11.4.240 |
+| Metadata File <br /> dotnet.inf | [INFO] <br /> [PART] <br /> file="dotnet.tar.bz2" <br /> version="5.0" <br /> size="200M" <br /> name="dotnet" <br /> minfw="11.04.240" |
+| Packing Notes | See build script for details |
+| Package automation | [build-dotnet-cp.sh](build-dotnet-cp.sh) |
---
+++ CP_Source/Apps/Microsoft_NET_Runtime/build-dotnet-cp.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#set -x
+#trap read debug
+
+# Creating an IGELOS CP for Microsoft .NET 5.0 Runtime
+## Development machine (Ubuntu 18.04)
+sudo apt install unzip -y
+wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update
+
+MISSING_LIBS="aspnetcore-runtime-5.0 dotnet-host dotnet-hostfxr-5.0 dotnet-runtime-5.0 dotnet-runtime-deps-5.0"
+
+mkdir build_tar
+cd build_tar
+
+for lib in $MISSING_LIBS; do
+  apt-get download $lib
+done
+
+mkdir -p custom/dotnet
+
+find . -name "*.deb" | while read LINE
+do
+  dpkg -x "${LINE}" custom/dotnet
+done
+
+wget https://github.com/IGEL-Community/IGEL-Custom-Partitions/raw/master/CP_Packages/Apps/Microsoft_NET_Runtime.zip
+
+unzip Microsoft_NET_Runtime.zip -d custom
+mv custom/target/dotnet-cp-init-script.sh custom
+
+cd custom
+
+tar cvjf dotnet.tar.bz2 dotnet dotnet-cp-init-script.sh
+mv dotnet.tar.bz2 ../..
+mv target/dotnet.inf ../..
+mv igel/*.xml ../..
+
+cd ../..
+rm -rf build_tar
