b69c7686c232a553b861640ac552e778eabb285b
neher-igel <neher@igel.com>
2021-07-26 20:12:25 -0600

    Aliza MS DICOM Viewer (26 July)

---
+++ CP_Source/Apps/Aliza_MS_DICOM_Viewer/README.md
@@ -0,0 +1,14 @@
+# Aliza MS DICOM Viewer (26 July)
+
+|  CP Information |            |
+|------------------|------------|
+| Package | [Aliza MS DICOM Viewer](https://www.aliza-dicom-viewer.com/home) <br /><br /> Aliza Medical Imaging. <br /><br /> [GitHub Dowload](https://github.com/AlizaMedicalImaging/AlizaMS/releases)|
+| Script Name | [alizams-cp-init-script.sh](alizams-cp-init-script.sh) |
+| CP Mount Path | /custom/alizams |
+| CP Size | 100M |
+| IGEL OS Version (min) | 11.04.240 |
+| Metadata File <br /> alizams.inf | [INFO] <br /> [PART] <br /> file="alizams.tar.bz2" <br /> version="1.6.0" <br /> size="100M" <br /> name="alizams" <br /> minfw="11.04.240" |
+| Path to Executable | /usr/local/alizams/alizams.sh |
+| Path to Icon | /usr/share/icons/hicolor/256x256/apps/alizams.png |
+| Packing Notes | See build script for details |
+| Package automation | [build-alizams-cp.sh](build-alizams-cp.sh) <br /><br /> Tested with 1.6.0 |
---
+++ CP_Source/Apps/Aliza_MS_DICOM_Viewer/build-alizams-cp.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#set -x
+#trap read debug
+
+# Creating an IGELOS CP for Aliza
+## Development machine (Ubuntu 18.04)
+# Obtain latest package and save into Downloads
+# Download Latest App for Linux (Debian)
+# https://github.com/AlizaMedicalImaging/AlizaMS/releases
+if ! compgen -G "$HOME/Downloads/alizams-*_linux.tar.gz" > /dev/null; then
+  echo "***********"
+  echo "Obtain latest alizams-*_linux.tar.gz package, save into $HOME/Downloads and re-run this script "
+  echo "https://github.com/AlizaMedicalImaging/AlizaMS/releases"
+  echo "***********"
+  exit 1
+fi
+
+sudo apt install unzip -y
+
+mkdir build_tar
+cd build_tar
+
+mkdir -p custom/alizams/usr/local
+
+cd custom/alizams/usr/local
+tar xvf $HOME/Downloads/alizams-*_linux.tar.gz
+#mv alizams-*_linux/* ../alizams
+cd ../..
+mkdir -p usr/share/applications.mime
+mkdir -p usr/share/icons
+mv usr/local/alizams-*_linux usr/local/alizams
+cp -r usr/local/alizams/install_menu/icons/* usr/share/icons
+
+# Create MimeType file
+cat << EOF >> usr/share/applications.mime/alizams.desktop
+[Desktop Entry]
+Type=Application
+Encoding=UTF-8
+Name=AlizaMS
+GenericName=AlizaMS
+Comment=Medical Imaging
+Exec="/custom/alizams/usr/local/alizams/alizams.sh" %F
+Icon=alizams
+Terminal=false
+Categories=Graphics;
+StartupNotify=false
+MimeType=application/dicom;
+EOF
+
+cd ../..
+
+wget https://github.com/IGEL-Community/IGEL-Custom-Partitions/raw/master/CP_Packages/Apps/Aliza_MS_DICOM_Viewer.zip
+
+unzip Aliza_MS_DICOM_Viewer.zip -d custom
+mv custom/target/aliza-cp-init-script.sh custom
+
+cd custom
+
+tar cvjf vmrc.tar.bz2 vmrc vmrc-cp-init-script.sh
+mv vmrc.tar.bz2 ../..
+mv target/vmrc.inf ../..
+mv igel/*.xml ../..
+
+cd ../..
+rm -rf build_tar
