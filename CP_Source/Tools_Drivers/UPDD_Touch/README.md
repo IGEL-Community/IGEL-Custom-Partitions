9382a7c103fda969b746a7f879642d708fee82dc
neher-igel <neher@igel.com>
2021-08-12 11:33:11 -0500

    UPDD Touch Driver (12 August - Development)

---
+++ CP_Source/Tools_Drivers/UPDD_Touch/README.md
@@ -0,0 +1,12 @@
+# UPDD Touch Drive (12 August - Development)
+
+|  CP Information |            |
+|------------------|------------|
+| Package | [UPDD Touch Driver](https://touch-base.com/oem/microchip/) <br /><br /> [UPDD provides a touch solution for Linux systems. This implementation supports the Linux operating system on X86 and X64 hardware](http://support.touch-base.com/Documentation/50346/Linux) |
+| Script Name | [updd-cp-init-script.sh](updd-cp-init-script.sh) |
+| CP Mount Path | /custom/updd |
+| CP Size | 200M |
+| IGEL OS Version (min) | 11.05.133 |
+| Metadata File <br /> updd.inf | [INFO] <br /> [PART] <br /> file="updd.tar.bz2" <br /> version="5_4-06_00_636" <br /> size="200M" <br /> name="updd" <br /> minfw="11.05.133" |
+| Packing Notes | See build script for details |
+| Package automation | [build-updd-cp.sh](build-updd-cp.sh) |
---
+++ CP_Source/Tools_Drivers/UPDD_Touch/build-updd-cp.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#set -x
+#trap read debug
+
+# Creating an IGELOS CP for UPDD Touch Driver
+## Development machine (Ubuntu 18.04)
+# Obtain latest package and save into Downloads
+# Download Latest App for Linux (Debian)
+# https://touch-base.com/oem/microchip/
+if ! compgen -G "$HOME/Downloads/linux_x86_gcc_*.tgz" > /dev/null; then
+  echo "***********"
+  echo "Obtain latest linux_x86_gcc_*.tgz package, save into $HOME/Downloads and re-run this script "
+  echo "https://touch-base.com/oem/microchip/"
+  echo "***********"
+  exit 1
+fi
+
+sudo apt install unzip -y
+
+mkdir build_tar
+cd build_tar
+
+mkdir -p custom/updd
+
+# http://support.touch-base.com/Documentation/50372/Installation
+mkdir installer
+cd installer
+tar xvzf $HOME/Downloads/linux_x86_gcc_*.tgz
+sudo ./install
+cd ..
+
+#tar cvf ../after_install.tar /opt/updd /etc/systemd/system/updd.* /etc/xdg/autostart/updd_*
+tar cvf after_install.tar /opt/updd /etc/xdg/autostart/updd_*
+
+cd custom/updd
+tar xvf after_install.tar
+
+cd ../..
+
+wget https://github.com/IGEL-Community/IGEL-Custom-Partitions/raw/master/CP_Packages/Tools_Drivers/UPDD_Touch.zip
+
+unzip UPDD_Touch.zip -d custom
+mv custom/target/updd-cp-init-script.sh custom
+
+cd custom
+
+tar cvjf updd.tar.bz2 updd updd-cp-init-script.sh
+mv updd.tar.bz2 ../..
+mv target/updd.inf ../..
+mv igel/*.xml ../..
+
+cd ../..
+rm -rf build_tar
