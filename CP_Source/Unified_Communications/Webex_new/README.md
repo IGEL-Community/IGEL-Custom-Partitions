b1883a3dba54c7a9e09d8eff58f7e23efe9d01ff
neher-igel <neher@igel.com>
2021-12-01 16:07:58 -0600

    Webex (new format)

---
+++ CP_Source/Unified_Communications/Webex_new/README.md
@@ -0,0 +1,12 @@
+# Cisco Webex (1 December) (new format)
+
+|  CP Information |            |
+|-----------------|------------|
+| Package | [Cisco Webex Client for Linux](https://help.webex.com/en-us/9vstcdb/Webex-for-Linux) All the core Webex capabilities in a single app are supported to help you seamlessly collaborate. This is just the beginning; support for advanced calling and a full-featured meeting experience will be added in upcoming releases. |
+| Script Name | [webex-cp-init-script.sh](build/webex-cp-init-script.sh) |
+| CP Mount Path | /custom/webex |
+| CP Size | 900M |
+| IGEL OS Version (min) | 11.4.240 |
+| Tested with version | 24 November 2021 (41.11.0.20717)
+| Packaging Notes | Details can be found in the build script |
+| Package automation | [build-webex-cp.sh](build/build-webex-cp.sh) <br /><br /> This script will build the latest version based on Ubuntu 18.04 |
---
+++ CP_Source/Unified_Communications/Webex_new/build/build-webex-cp.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+#set -x
+#trap read debug
+
+# Creating an IGELOS CP for Webex
+## Development machine (Ubuntu 18.04)
+# Obtain latest package and save into Downloads
+# Download Latest App for Linux (Debian)
+#https://www.webex.com/downloads.html
+if ! compgen -G "$HOME/Downloads/Webex*.deb" > /dev/null; then
+  echo "***********"
+  echo "Obtain latest .deb package, save into $HOME/Downloads and re-run this script "
+  echo "https://www.webex.com/downloads.html"
+  echo "***********"
+  exit 1
+fi
+
+MISSING_LIBS="libxcb-xinerama0"
+
+sudo apt install unzip -y
+
+mkdir build_tar
+cd build_tar
+
+for lib in $MISSING_LIBS; do
+  apt-get download $lib
+done
+
+mkdir -p custom/webex
+
+dpkg -x $HOME/Downloads/Webex*.deb custom/webex
+
+find . -type f -name "*.deb" | while read LINE
+do
+  dpkg -x "${LINE}" custom/webex
+done
+
+mkdir -p custom/webex/usr/share/applications.mime
+cp custom/webex/opt/Webex/bin/webex.desktop custom/webex/usr/share/applications.mime
+
+############################################
+# START: comment out for non-persistency!!!!
+############################################
+
+mkdir -p custom/webex/userhome/.local/share/Webex
+
+##########################################
+# END: comment out for non-persistency!!!!
+##########################################
+
+wget https://github.com/IGEL-Community/IGEL-Custom-Partitions/raw/master/CP_Packages/Unified_Communications/Webex_new.zip
+
+unzip Webex_new.zip -d custom
+mkdir -p custom/webex/config/bin
+mkdir -p custom/webex/lib/systemd/system
+mv custom/target/build/webex_cp_apparmor_reload custom/webex/config/bin
+mv custom/target/build/igel-webex-cp-apparmor-reload.service custom/webex/lib/systemd/system/
+mv custom/target/build/webex-cp-init-script.sh custom
+
+cd custom
+
+# edit inf file for version number
+mkdir getversion
+cd getversion
+ar -x $HOME/Downloads/Webex*.deb
+tar xf control.tar.gz ./control
+VERSION=$(grep Version control | cut -d " " -f 2)
+#echo "Version is: " ${VERSION}
+cd ..
+sed -i "/^version=/c version=\"${VERSION}\"" target/webex.inf
+#echo "webex.inf file is:"
+#cat target/webex.inf
+
+# new build process into zip file
+tar cvjf target/webex.tar.bz2 webex webex-cp-init-script.sh
+zip -g ../Webex_new.zip target/webex.tar.bz2 target/webex.inf
+zip -d ../Webex_new.zip "target/build/*" "target/igel/*" "target/target/*"
+mv ../Webex_new.zip ../../Webex-${VERSION}_igel01.zip
+
+cd ../..
+rm -rf build_tar
