942240967b6fe032c2369b20fa43edd402cd1aa8
neher-igel <neher@igel.com>
2021-12-01 15:26:51 -0600

    Microsoft Teams (new format)

---
+++ CP_Source/Unified_Communications/Microsoft_Teams_new_format/build/build-teams-cp.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#set -x
+#trap read debug
+
+# Creating an IGELOS CP for Microsoft Teams
+## Development machine (Ubuntu 18.04)
+sudo apt install curl -y
+sudo apt install unzip -y
+sudo curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/ms-teams stable main" > /etc/apt/sources.list.d/teams.list'
+sudo apt-get update
+
+mkdir build_tar
+cd build_tar
+
+apt-get download teams
+apt-get download libgnome-keyring0
+
+mkdir -p custom/teams
+
+dpkg -x teams_* custom/teams
+dpkg -x libg* custom/teams
+
+mv custom/teams/usr/share/applications/ custom/teams/usr/share/applications.mime
+mkdir -p custom/teams/userhome/.config/Microsoft
+
+wget https://github.com/IGEL-Community/IGEL-Custom-Partitions/raw/master/CP_Packages/Unified_Communications/Microsoft_Teams_new_format.zip
+
+unzip Microsoft_Teams_new_format.zip -d custom
+mkdir -p custom/teams/config/bin
+mkdir -p custom/teams/lib/systemd/system
+mv custom/target/teams_cp_apparmor_reload custom/teams/config/bin
+mv custom/target/igel-teams-cp-apparmor-reload.service custom/teams/lib/systemd/system/
+mv custom/target/teams-cp-init-script.sh custom
+
+cd custom
+
+# edit inf file for version number
+mkdir getversion
+cd getversion
+ar -x ../../teams_*
+tar xf control.tar.xz ./control
+VERSION=$(grep Version control | cut -d " " -f 2)
+#echo "Version is: " ${VERSION}
+cd ..
+sed -i "/^version=/c version=\"${VERSION}\"" target/teams.inf
+#echo "teams.inf file is:"
+#cat target/teams.inf
+
+# new build process into zip file
+tar cvjf target/teams.tar.bz2 teams teams-cp-init-script.sh
+zip -g ../Microsoft_Teams_new_format.zip target/teams.tar.bz2 target/teams.inf
+zip -d ../Microsoft_Teams_new_format.zip "target/build/*" "target/igel/*" "target/target/*"
+mv ../Microsoft_Teams_new_format.zip ../../Microsoft_Teams-${VERSION}_igel01.zip
+
+cd ../..
+rm -rf build_tar
---
+++ CP_Source/Unified_Communications/Microsoft_Teams_new_format/build/teams-cp-init-script.sh
@@ -0,0 +1,70 @@
+#! /bin/bash
+#set -x
+#trap read debug
+
+ACTION="custompart-teams_${1}"
+
+# mount point path
+MP=$(get custom_partition.mountpoint)
+
+# custom partition path
+CP="${MP}/teams"
+
+# Teams directory
+TEAMS="/userhome/.config/Microsoft"
+
+# output to systemlog with ID amd tag
+LOGGER="logger -it ${ACTION}"
+
+echo "Starting" | $LOGGER
+
+case "$1" in
+init)
+  # Linking files and folders on proper path
+  find ${CP} | while read LINE
+  do
+    DEST=$(echo -n "${LINE}" | sed -e "s|${CP}||g")
+    if [ ! -z "${DEST}" -a ! -e "${DEST}" ]; then
+      # Remove the last slash, if it is a dir
+      [ -d $LINE ] && DEST=$(echo "${DEST}" | sed -e "s/\/$//g") | $LOGGER
+      if [ ! -z "${DEST}" ]; then
+        ln -sv "${LINE}" "${DEST}" | $LOGGER
+      fi
+    fi
+  done
+
+  # basic persistency
+  chown -R user:users "${CP}${TEAMS}"
+
+  # add MIME type to /usr/share/applications/mimeapps.list
+  echo "x-scheme-handler/msteams=teams.desktop" >> /usr/share/applications/mimeapps.list
+
+  # Add apparmor profile to trust Teams in Firefox to make SSO possible
+  # We do this by a systemd service to run the reconfiguration
+  # surely after apparmor.service!!!
+  systemctl --no-block start igel-teams-cp-apparmor-reload.service
+
+  # after CP installation run wm_postsetup to activate teams.desktop mimetypes for SSO
+  if [ -d /run/user/777 ]; then
+    wm_postsetup
+    # delay the CP ready notification
+    sleep 3
+  fi
+
+  ldconfig
+
+;;
+stop)
+  # unlink linked files
+  find ${CP} | while read LINE
+  do
+    DEST=$(echo -n "${LINE}" | sed -e "s|${CP}||g")
+    unlink $DEST | $LOGGER
+  done
+
+;;
+esac
+
+echo "Finished" | $LOGGER
+
+exit 0
