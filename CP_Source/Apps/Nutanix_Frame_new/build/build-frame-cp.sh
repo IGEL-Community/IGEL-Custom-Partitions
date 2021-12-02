1378d4e64c3bfe7d5c4f8aa983415de7a5321b4b
neher-igel <neher@igel.com>
2021-12-02 14:42:34 -0600

    Nutanix Frame (2 December) (new format)

--- CP_Source/Apps/Nutanix_Frame_new/build/build-frame-cp.sh
+++ CP_Source/Apps/Nutanix_Frame_new/build/build-frame-cp.sh
@@ -39,7 +39,7 @@ cd custom
 mkdir getversion
 cd getversion
 ar -x $HOME/Downloads/Frame-*.deb
-tar xf control.tar.xz ./control
+tar xf control.tar.* ./control
 VERSION=$(grep Version control | cut -d " " -f 2)
 #echo "Version is: " ${VERSION}
 cd ..
