7ba0e43aa87ae93dc3bc86732f681f3a970c436b
neher-igel <neher@igel.com>
2021-12-18 12:55:44 -0600

    LibreOffice (18 December)

---
+++ CP_Source/Office/LibreOffice_new/README.md
@@ -0,0 +1,18 @@
+# LibreOffice (18 December)
+
+|  CP Information |            |
+|--------------------|------------|
+| Package | [LibreOffice](https://www.libreoffice.org) - Current Version <br /><br /> [Installing LibreOffice on Linux](https://wiki.documentfoundation.org/Documentation/Install/Linux) <br /><br /> [LibreOffice Help](https://help.libreoffice.org)|
+| CP Mount Path | /custom/libreoffice |
+| CP Size | 900M |
+| IGEL OS Version (min) | 11.05.133 |
+| Path to Executable | /custom/libreoffice/opt/libreoffice7.2/program/soffice |
+| Path to Icon | /custom/libreoffice/usr/share/icons/hicolor/256x256/apps/libreoffice7.2-main.png |
+| Packaging Notes | See build script for details. |
+| Package automation | [build-libreoffice-cp.sh](build-libreoffice-cp.sh) <br /><br /> Tested with 7.2.X |
+
+**NOTES**
+
+- For Help to open files in browser, you will need to enable browser (such as Firefox), allow the browser to read files on the filesystem, and disable **system.security.apparmor**.
+
+- The **version number is embedded in path** (currently 7.2) and references to 7.2 will need to be modified when 7.3 or later versions are released!
---
+++ CP_Source/Office/LibreOffice_new/build/build-libreoffice-cp.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+#set -x
+#trap read debug
+
+# Creating an IGELOS CP for LibreOffice
+## Development machine (Ubuntu 18.04)
+
+# Obtain latest package and save into Downloads
+# Download Latest App for Linux (Debian)
+# https://www.libreoffice.org/download/download/?type=deb-x86_64
+if ! compgen -G "$HOME/Downloads/LibreOffice_*_Linux_x86-64_deb.tar.gz" > /dev/null; then
+  echo "***********"
+  echo "Obtain latest Linux (64-bit) (deb) package, save into $HOME/Downloads and re-run this script "
+  echo "https://www.libreoffice.org/download/download/?type=deb-x86_64"
+  echo "***********"
+  exit 1
+fi
+if ! compgen -G "$HOME/Downloads/LibreOffice_*_Linux_x86-64_deb_helppack_*.tar.gz" > /dev/null; then
+  echo "***********"
+  echo "Help for offline use"
+  echo "Obtain latest Linux (64-bit) (deb) package, save into $HOME/Downloads and re-run this script "
+  echo "https://www.libreoffice.org/download/download/?type=deb-x86_64"
+  echo "***********"
+  exit 1
+fi
+
+sudo apt install unzip -y
+
+mkdir build_tar
+cd build_tar
+
+mkdir -p custom/libreoffice
+
+tar zxvf $HOME/Downloads/LibreOffice_*_Linux_x86-64_deb.tar.gz
+LIBREOFFICEDIR="LibreOffice_*_Linux_x86-64_deb/DEBS"
+find ${LIBREOFFICEDIR} -type f | while read LINE
+do
+  dpkg -x "${LINE}" custom/libreoffice
+done
+
+tar zxvf $HOME/Downloads/LibreOffice_*_Linux_x86-64_deb_helppack_*.tar.gz
+dpkg -x LibreOffice_*_Linux_x86-64_deb_helppack_*/DEBS/lib*.deb custom/libreoffice
+
+mv custom/libreoffice/usr/share/applications/ custom/libreoffice/usr/share/applications.mime
+mkdir -p custom/libreoffice/userhome/.config/libreoffice
+mkdir -p custom/libreoffice/userhome/Documents
+
+wget https://github.com/IGEL-Community/IGEL-Custom-Partitions/raw/master/CP_Packages/Office/LibreOffice_new.zip
+
+unzip LibreOffice_new.zip -d custom
+mkdir -p custom/libreoffice/config/bin
+mkdir -p custom/libreoffice/lib/systemd/system
+mv custom/target/build/libreoffice_cp_apparmor_reload custom/libreoffice/config/bin
+mv custom/target/build/igel-libreoffice-cp-apparmor-reload.service custom/libreoffice/lib/systemd/system/
+mv custom/target/build/libreoffice-cp-init-script.sh custom
+
+cd custom
+
+# edit inf file for version number
+mkdir getversion
+cd getversion
+ar -x "../../LibreOffice_*/DEBS/libreoffice*-base_*.deb"
+tar xf control.tar.*
+VERSION=$(grep Version control | cut -d " " -f 2)
+#echo "Version is: " ${VERSION}
+cd ..
+sed -i "/^version=/c version=\"${VERSION}\"" target/libreoffice.inf
+#echo "libreoffice.inf file is:"
+#cat target/libreoffice.inf
+
+# new build process into zip file
+tar cvjf target/libreoffice.tar.bz2 libreoffice libreoffice-cp-init-script.sh
+zip -g ../.zip target/libreoffice.tar.bz2 target/libreoffice.inf
+zip -d ../LibreOffice_new.zip "target/build/*" "target/igel/*" "target/target/*"
+mv ../LibreOffice_new.zip ../../LibreOffice_new-${VERSION}_igel01.zip
+
+cd ../..
+rm -rf build_tar
