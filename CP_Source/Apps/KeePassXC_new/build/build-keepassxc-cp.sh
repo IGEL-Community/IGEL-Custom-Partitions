71df01f5bd3088a5df884164024fdef92bee005d
neher-igel <neher@igel.com>
2021-12-18 17:06:46 -0600

    KeePassXC (18 December)

---
+++ CP_Source/Apps/KeePassXC_new/build/build-keepassxc-cp.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+#set -x
+#trap read debug
+
+# Creating an IGELOS CP for keepassxc
+## Development machine (Ubuntu 18.04)
+
+#sudo add-apt-repository ppa:phoerious/keepassxc
+sudo sh -c 'echo "deb [arch=amd64] http://ppa.launchpad.net/phoerious/keepassxc/ubuntu bionic main" > /etc/apt/sources.list.d/phoerious-ubuntu-keepassxc-bionic.list'
+sudo apt-get update
+MISSING_LIBS="keepassxc libdouble-conversion1 libqrencode3 libqt5concurrent5 libqt5core5a libqt5dbus5 libqt5gui5 libqt5network5 libqt5svg5 libqt5widgets5 libqt5x11extras5 libquazip5-1 libxcb-xinerama0 libykpers-1-1 libyubikey0 libzxcvbn0 qt5-gtk-platformtheme qttranslations5-l10n"
+
+sudo apt install unzip -y
+
+mkdir build_tar
+cd build_tar
+
+for lib in $MISSING_LIBS; do
+  apt-get download $lib
+done
+
+mkdir -p custom/keepassxc
+
+find . -type f -name "*.deb" | while read LINE
+do
+  dpkg -x "${LINE}" custom/keepassxc
+done
+
+mv custom/keepassxc/usr/share/applications/ custom/keepassxc/usr/share/applications.mime
+
+echo "+++++++=======  STARTING CLEAN of USR =======+++++++"
+wget https://raw.githubusercontent.com/IGEL-Community/IGEL-Custom-Partitions/master/utils/igelos_usr/clean_cp_usr_lib.sh
+chmod a+x clean_cp_usr_lib.sh
+wget https://raw.githubusercontent.com/IGEL-Community/IGEL-Custom-Partitions/master/utils/igelos_usr/clean_cp_usr_share.sh
+chmod a+x clean_cp_usr_share.sh
+./clean_cp_usr_lib.sh 11.05.133_usr_lib.txt custom/keepassxc/usr/lib
+./clean_cp_usr_share.sh 11.05.133_usr_share.txt custom/keepassxc/usr/share
+echo "+++++++=======  DONE CLEAN of USR =======+++++++"
+
+mkdir -p custom/keepassxc/userhome/.config/keepassxc
+mkdir -p custom/keepassxc/userhome/KeePassXCDB
+
+wget https://github.com/IGEL-Community/IGEL-Custom-Partitions/raw/master/CP_Packages/Apps/KeePassXC_new.zip
+
+unzip KeePassXC_new.zip -d custom
+mv custom/target/build/keepassxc-cp-init-script.sh custom
+
+cd custom
+
+# edit inf file for version number
+mkdir getversion
+cd getversion
+ar -x ../../keepassxc*.deb
+tar xf control.tar.*
+VERSION=$(grep Version control | cut -d " " -f 2)
+#echo "Version is: " ${VERSION}
+cd ..
+sed -i "/^version=/c version=\"${VERSION}\"" target/keepassxc.inf
+#echo "keepassxc.inf file is:"
+#cat target/keepassxc.inf
+
+# new build process into zip file
+tar cvjf target/keepassxc.tar.bz2 keepassxc keepassxc-cp-init-script.sh
+zip -g ../KeePassXC_new.zip target/keepassxc.tar.bz2 target/keepassxc.inf
+zip -d ../KeePassXC_new.zip "target/build/*" "target/igel/*" "target/target/*"
+mv ../KeePassXC_new.zip ../../KeePassXC_new-${VERSION}_igel01.zip
+
+cd ../..
+rm -rf build_tar
---
+++ CP_Source/Apps/KeePassXC_new/build/keepassxc-cp-init-script.sh
@@ -0,0 +1,76 @@
+#! /bin/bash
+#set -x
+#trap read debug
+
+ACTION="custompart-keepassxc_${1}"
+
+# mount point path
+MP=$(get custom_partition.mountpoint)
+
+# custom partition path
+CP="${MP}/keepassxc"
+
+# userhome
+USER_CONFIG="/userhome/.config/keepassxc"
+KEEPASS_DB="/userhome/KeePassXCDB"
+
+# output to systemlog with ID amd tag
+LOGGER="logger -it ${ACTION}"
+
+echo "Starting" | $LOGGER
+
+case "$1" in
+init)
+  if [ -L ${USER_CONFIG} ]; then
+    unlink ${USER_CONFIG}
+  fi
+  if [ -d ${USER_CONFIG} ]; then
+    rm -rf ${USER_CONFIG}
+  fi
+  if [ -L ${KEEPASS_DB} ]; then
+    unlink ${KEEPASS_DB}
+  fi
+  if [ -d ${KEEPASS_DB} ]; then
+    rm -rf ${KEEPASS_DB}
+  fi
+  # Linking files and folders on proper path
+  find ${CP} | while read LINE
+  do
+    DEST=$(echo -n "${LINE}" | sed -e "s|${CP}||g")
+    if [ ! -z "${DEST}" -a ! -e "${DEST}" ]; then
+      # Remove the last slash, if it is a dir
+      [ -d $LINE ] && DEST=$(echo "${DEST}" | sed -e "s/\/$//g") | $LOGGER
+      if [ ! -z "${DEST}" ]; then
+        ln -sv "${LINE}" "${DEST}" | $LOGGER
+      fi
+    fi
+  done
+
+  # basic persistency
+  chown -R user:users "${CP}${KEEPASS_CONFIG}"
+  chown -R user:users "${CP}${KEEPASS_DB}"
+
+  # after CP installation run wm_postsetup to activate mimetypes for SSO
+  if [ -d /run/user/777 ]; then
+    wm_postsetup
+    # delay the CP ready notification
+    sleep 3
+  fi
+
+  ldconfig
+
+;;
+stop)
+  # unlink linked files
+  find ${CP} | while read LINE
+  do
+    DEST=$(echo -n "${LINE}" | sed -e "s|${CP}||g")
+    unlink $DEST | $LOGGER
+  done
+
+;;
+esac
+
+echo "Finished" | $LOGGER
+
+exit 0
