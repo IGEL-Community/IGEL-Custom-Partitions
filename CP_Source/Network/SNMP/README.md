58a980926db35f091ebb993c63fe9e210f8b148c
neher-igel <neher@igel.com>
2022-05-02 08:20:19 -0600

    SNMP (2 May)

---
+++ CP_Source/Network/SNMP/README.md
@@ -0,0 +1,14 @@
+# SNMP (1 May)
+
+|  CP Information |            |
+|-----------------|------------|
+| Package | SNMP |
+| Script Name | [snmp-cp-init-script.sh](build/snmp-cp-init-script.sh) |
+| CP Mount Path | /custom/snmp |
+| CP Size | 100M |
+| IGEL OS Version (min) | 11.05.133 |
+| Packaging Notes | Details can be found in the build script [build-snmp-cp.sh](build/build-snmp-cp.sh) |
+
+**NOTES:**
+
+- A reboot is required before the client can start.
---
+++ CP_Source/Network/SNMP/build/snmp-cp-init-script.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+#set -x
+#trap read debug
+
+ACTION="custompart-snmp_${1}"
+
+# mount point path
+MP=$(get custom_partition.mountpoint)
+
+# custom partition path
+CP="${MP}/snmp"
+
+# output to systemlog with ID amd tag
+LOGGER="logger -it ${ACTION}"
+
+echo "Starting" | $LOGGER
+
+case "$1" in
+init)
+  # Initial permissions
+  chown -R root:root "${CP}" | $LOGGER
+  # Linking files and folders on proper path
+  find ${CP} | while read LINE
+  do
+    DEST=$(echo -n "${LINE}" | sed -e "s|${CP}||g")
+    if [ ! -z "${DEST}" -a ! -e "${DEST}" ]; then
+      # Remove the last slash, if it is a dir
+      [ -d $LINE ] && DEST=$(echo "${DEST}" | sed -e "s/\/$//g") | $LOGGER
+      if [ ! -z "${DEST}" ]; then
+        ln -sv "${LINE}" "${DEST}" | $LOGGER
+      fi
+    fi
+  done
+
+;;
+stop)
+  # unlink linked files
+  find ${CP} | while read LINE
+  do
+    DEST=$(echo -n "${LINE}" | sed -e "s|${CP}||g")
+    unlink $DEST | $LOGGER
+  done
+
+;;
+esac
+
+echo "Finished" | $LOGGER
+
+exit 0
