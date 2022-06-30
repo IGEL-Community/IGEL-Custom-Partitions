df3fd601c79ae2b3b700177fb15878263b22e04c
Jason <jason.thompson@nutanix.com>
2022-06-30 08:19:22 -0700

    Update frame-launcher.sh
    + Added more comments to the script that explain the end user workflow supported by this script and the Frame configuration assumptions.
    + Removed the `--force-gpu` flag and added the `--displays-auto-arrange` option.
    + Fixed typos.

--- CP_Source/Apps/Nutanix_Frame/build/frame-launcher.sh
+++ CP_Source/Apps/Nutanix_Frame/build/frame-launcher.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# This script implements a specific end user experience workflow and assumes a particular Frame configuration.
+#
+# The following end user experience workflow is supported with this script:
+#
+#   1. End users do not need to authenticate to a SAML2-based identity provider (using the Frame Secure Anonymous Token (SAT) mechanism).
+#   2. User cache is removed prior to start of Frame App to ensure no user preference settings have persisted since the prior use of Frame App.
+#   3. Frame App will launch in "kiosk mode".
+#   4. End users will be taken by Frame App straight to the desktop or application.
+#   5. When a Frame session starts, the remote desktop will be in full-screen mode.
+#   6. When end users disconnect or closes session, end users are returned to the Launchpad designated by the Frame Customer Administrator (SAT Permission).
+#
+# Frame configuration assumptions:
+#
+#   1. API Provider at the Organization entity
+#   2. Secure Anonymous Provider at the Organization entity granting a role of Launchpad user for a specific Launchpad in a Frame account (under the Organization entity).
+#
+# Updated June 29, 2022
+
 # Check if all the required envs are set correctly
 if [ -z "${FRAME_CLIENT_ID}" ] || [ -z "${FRAME_CLIENT_SECRET}" ] || [ -z "${FRAME_ANONYMOUS_PROVIDER_NAME}" ] || [ -z "${FRAME_ORGANIZATION_ID}" ] || [ -z "${FRAME_EMAIL_DOMAIN}" ]; then
     echo "Nutanix Frame: Please fill FRAME_CLIENT_ID, FRAME_CLIENT_SECRET, FRAME_EMAIL_DOMAIN, FRAME_ORGANIZATION_ID and FRAME_ANONYMOUS_PROVIDER_NAME environment variables"
@@ -38,11 +56,13 @@ getToken() {
         openssl dgst -sha256 -hmac "$FRAME_CLIENT_SECRET" |
         sed 's/^.* //')
 
-    # Crete email address for anonymous user from baseboard serial number and env:FRAME_EMAIL_DOMAIN - e.g. ITC00152278@nutanix.com
+    # Create an email address for anonymous user from baseboard serial number and env:FRAME_EMAIL_DOMAIN - e.g. ITC00152278@nutanix.com. Value must be in an email format.
+    # Specific pattern defined by the customer for use in activity reports and custom Frame Guest Agent pre-session scripts.
     serial=$(dmidecode -s baseboard-serial-number)
     email="ITC${serial}@${FRAME_EMAIL_DOMAIN}"
 
-    # Include optional params such as first_name, last_name, email_domain, email or metadata
+    # Include optional parameter values such as first_name, last_name, email_domain, email or metadata which will be shown in Frame Dashboard and User Activity and Session Trail reports.
+    # Refer to https://docs.frame.nutanix.com/frame-apis/sat-api.html#anonymous-token-parameters for details.
     local request_body='{
         "metadata": {
             "should_accept_tos": false
@@ -79,15 +99,14 @@ setAutoLaunch() {
 
 }
 runFrame() {
-    # Clear cache from previous session
+    # Clear cache from the previous use of Frame App
     rm -Rf /custom/frame/userhome/.Nutanix/Frame/cache
 
-    # Run Frame terminal
-    #/custom/frame/usr/lib/frame/Frame --kiosk --force-gpu --url="$frame_url/#token=$token"
-    /custom/frame/usr/bin/nutanix-frame/Frame --kiosk --force-gpu --url="$frame_url/#token=$token"
+    # Run Frame App in kiosk mode (--kiosk), full screen when in a Frame session (--displays-auto-arrange), and start at the specified URL (--url) with a Secure Anonymous Token.
+    /custom/frame/usr/bin/nutanix-frame/Frame --kiosk --displays-auto-arrange --url="$frame_url/#token=$token"
 }
 
-# Obtain anonymous token
+# Obtain Secure Anonymous Token from Frame Control Plane
 getToken
 
 # Confirm that token is obtained succefully
@@ -96,7 +115,7 @@ if [ -z "$token" ]; then
     exit 1
 
 else
-    # Once we get the token we'll try to set autolaunch and enter the session
+    # Once we get the token, set autolaunch, start up Frame App, and enter the session.
     setAutoLaunch
     runFrame
 fi
