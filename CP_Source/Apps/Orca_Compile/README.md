507b51ae1ba22a372cfd72efa34faaa5527f6105
neher-igel <neher@igel.com>
2022-09-09 13:46:06 -0600

    Orca_Compile (9 September) (Dev Testing)

---
+++ CP_Source/Apps/Orca_Compile/README.md
@@ -0,0 +1,39 @@
+# Orca Screen Reader (Dev Testing) (9 September)
+
+|  CP Information |            |
+|--------------------|------------|
+| Package | [Orca Screen Reader](https://wiki.gnome.org/action/show/Projects/Orca?action=show&redirect=Orca) - Build current Version <br /><br /> Orca is a free, open source, flexible, and extensible screen reader that provides access to the graphical desktop via user-customizable combinations of speech and/or braille. <br /><br /> [Orca Screen Reader Overview](https://techblog.wikimedia.org/2020/07/02/an-orca-screen-reader-tutorial/) <br /><br /> [Orca GitHub](https://github.com/GNOME/orca) |
+| Script Name | [orca-cp-init-script.sh](orca-cp-init-script.sh) |
+| CP Mount Path | /custom/orca |
+| Packaging Notes | See build script |
+| Package automation | [build-orca-cp.sh](build/build-orca-cp.sh) |
+
+**Test espeak**
+
+```{test espeak}
+echo "Hello IGEL Community" | /usr/bin/espeak-ng
+  ```
+
+**Configure spd-conf**  
+
+```{configure spd-conf}
+/usr/bin/spd-conf
+  ```
+
+**Test spd-say**
+
+```{test spd-say}  
+/usr/bin/spd-say "Hello IGEL Community"
+  ```
+
+**Setup Orca**
+
+```{setup orca}  
+orca -s
+  ```
+
+**Command to start speech-dispatcher**
+
+```{start}
+/usr/bin/speech-dispatcher -d -t 300000
+  ```
---
+++ CP_Source/Apps/Orca_Compile/build/build-orca-cp.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+#set -x
+#trap read debug
+
+# Creating an IGELOS CP for Orca Screen Reader
+## Development machine (Ubuntu 18.04)
+MISSING_LIBS="asterisk-espeak brltty-espeak dconf-gsettings-backend emacspeak emacspeak-espeak-server espeak espeak-data espeak-ng espeak-ng-data espeak-ng-espeak espeakedit espeakup gespeaker gir1.2-atspi-2.0 gir1.2-glib-2.0 gir1.2-gtk-3.0 gir1.2-pango-1.0 gir1.2-wnck-3.0 gsettings-backend gsettings-desktop-schemas gstreamer1.0-espeak libao4 libatk-adaptor libatk-bridge2.0-0 libatspi2.0-0 libbrlapi0.6 libdotconf0 libespeak-ng-libespeak1 libespeak-ng1 libespeak1 liblouis14 libltdl7 libpcaudio0 libperlspeak-perl libportaudio2 libpulse0 libsndfile1 libsonic0 libspeechd2 pkg-config pypy-py python-espeak python-glade2 python-gtk2 python-gtk2-doc python-gtkspellcheck python-gtkspellcheck-doc python-py python-pyatspi python-strongwind python3-brlapi python3-cairo python3-espeak python3-gi python3-gtkspellcheck python3-louis python3-py python3-pyatspi python3-speechd python3-xdg speech-dispatcher speech-dispatcher-audio-plugins speech-dispatcher-espeak speech-dispatcher-espeak-ng speechd-up stardict-plugin-espeak xbrlapi"
+
+sudo apt install unzip -y
+
+sudo apt install -y autoconf automake autopoint build-essential libcurl4-openssl-dev libsqlite3-dev pkg-config git curl libnotify-dev yelp-tools pkg-config python-gi-dev python-glade2 python-gobject-2-dev python-gtk2 python-gtk2-dbg python-gtk2-dev python-gtk2-doc python-gtkspellcheck python-gtkspellcheck-doc python3-gtkspellcheck gir1.2-atspi-2.0 libatspi2.0-0 libatspi2.0-dev python3-pyatspi python-pyatspi python-strongwind libatk-bridge2.0-0 libatk-bridge2.0-dev
+
+mkdir -p build_tar/compile
+
+# START build Orca
+cd build_tar/compile
+git clone https://github.com/GNOME/orca.git
+cd orca
+./autogen.sh --prefix=`pwd`/bld && make && make install
+ORCA_BUILD_PATH=$(pwd)
+cd ../../..
+# END build Orca
+
+cd build_tar
+
+for lib in $MISSING_LIBS; do
+  apt-get download $lib
+done
+
+mkdir -p custom/orca
+
+# START copy Orca build
+mkdir -p custom/orca/usr/bin
+cp -R $ORCA_BUILD_PATH/bld/bin/* custom/orca/usr/bin
+mkdir -p custom/orca/usr/lib
+cp -R $ORCA_BUILD_PATH/bld/lib/* custom/orca/usr/lib
+mkdir -p custom/orca/usr/share
+cp -R $ORCA_BUILD_PATH/bld/share/* custom/orca/usr/share
+# END copy Orca build
+
+find . -type f -name "*.deb" | while read LINE
+do
+  dpkg -x "${LINE}" custom/orca
+done
+
+
+echo "+++++++=======  STARTING CLEAN of USR =======+++++++"
+wget https://raw.githubusercontent.com/IGEL-Community/IGEL-Custom-Partitions/master/utils/igelos_usr/clean_cp_usr_lib.sh
+chmod a+x clean_cp_usr_lib.sh
+wget https://raw.githubusercontent.com/IGEL-Community/IGEL-Custom-Partitions/master/utils/igelos_usr/clean_cp_usr_share.sh
+chmod a+x clean_cp_usr_share.sh
+./clean_cp_usr_lib.sh 11.07.100_usr_lib.txt custom/orca/usr/lib
+./clean_cp_usr_share.sh 11.07.100_usr_share.txt custom/orca/usr/share
+echo "+++++++=======  DONE CLEAN of USR =======+++++++"
+
+wget https://github.com/IGEL-Community/IGEL-Custom-Partitions/raw/master/CP_Packages/Apps/Orca_Compile.zip
+
+unzip Orca_Compile.zip -d custom
+
+mv custom/target/build/orca-cp-init-script.sh custom
+
+cd custom
+
+# edit inf file for version number
+VERSION=$(grep "^VERSION = " $ORCA_BUILD_PATH/Makefile | cut -d " " -f 3)
+#echo "Version is: " ${VERSION}
+sed -i "/^version=/c version=\"${VERSION}\"" target/orca.inf
+#echo "orca.inf file is:"
+#cat target/orca.inf
+
+# new build process into zip file
+tar cvjf target/orca.tar.bz2 orca orca-cp-init-script.sh
+zip -g ../Orca_Compile.zip target/orca.tar.bz2 target/orca.inf
+zip -d ../Orca_Compile.zip "target/build/*" "target/igel/*" "target/target/*"
+mv ../Orca_Compile.zip ../../Orca_Compile-${VERSION}_igel01.zip
+
+cd ../..
+rm -rf build_tar
