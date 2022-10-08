d2ca47d3c30cfd056549ce5e040da2e42b3770d0
neher-igel <neher@igel.com>
2022-10-08 10:28:22 -0600

    exacqVision Client (8 October)

---
+++ CP_Source/Apps/ExacqVision_Client/README.md
@@ -0,0 +1,11 @@
+# exacqVision Client (8 October)
+
+| CP Information        |                                                 |
+| --------------------- | ------------------------------------------------|
+| Package               | [exacqVision Professional](https://www.exacq.com/products/professional/) Powerful, Intuitive Video Management Software |
+| Script Name           | [exacqvision-cp-init-script.sh](build/exacqvision-cp-init-script.sh) |
+| CP Mount Path         | /custom/exacqvision                             |
+| CP Size               | 500M                                            |
+| IGEL OS Version (min) | 11.07.110                                       |
+| Packaging Notes       | See build script for details                    |
+| Package automation    | [build-exacqvision-cp.sh](build/build-exacqvision-cp.sh) |
---
+++ CP_Source/Apps/ExacqVision_Client/build/exacqvision-cp-init-script.sh
@@ -0,0 +1,64 @@
+#! /bin/bash
+#set -x
+#trap read debug
+
+ACTION="custompart-exacqvision_${1}"
+
+# mount point path
+MP=$(get custom_partition.mountpoint)
+
+# custom partition path
+CP="${MP}/exacqvision"
+
+# config directory
+USER_CONFIG="/userhome/.edvrclient.dir"
+
+# output to systemlog with ID amd tag
+LOGGER="logger -it ${ACTION}"
+
+echo "Starting" | $LOGGER
+
+case "$1" in
+init)
+  # check for old folders / links and remove
+  if [ -L ${USER_CONFIG} ]; then
+    unlink ${USER_CONFIG}
+  fi
+  if [ -d ${USER_CONFIG} ]; then
+    rm -rf ${USER_CONFIG}
+  fi
+  # Initial permissions
+  chown -R root:root "${CP}" | $LOGGER
+  # Linking files and folders on proper path
+  find ${CP} | while read LINE
+  do
+    DEST=$(echo -n "${LINE}" | sed -e "s|${CP}||g")
+    if [ ! -z "${DEST}" -a ! -e "${DEST}" ]; then
+      # Remove the last slash, if it is a dir
+      [ -d $LINE ] && DEST=$(echo "${DEST}" | sed -e "s/\/$//g") | $LOGGER
+      if [ ! -z "${DEST}" ]; then
+        ln -sv "${LINE}" "${DEST}" | $LOGGER
+      fi
+    fi
+  done
+
+  # basic persistency
+  if [ -d "${CP}${USER_CONFIG}" ]; then
+    chown -R user:users "${CP}${USER_CONFIG}"
+  fi
+
+;;
+stop)
+  # unlink linked files
+  find ${CP} | while read LINE
+  do
+    DEST=$(echo -n "${LINE}" | sed -e "s|${CP}||g")
+    unlink $DEST | $LOGGER
+  done
+
+;;
+esac
+
+echo "Finished" | $LOGGER
+
+exit 0
