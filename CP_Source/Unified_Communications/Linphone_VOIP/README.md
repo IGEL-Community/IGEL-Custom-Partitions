63cd44c7e83a2ecccc89a48be0a556d8e03bc5f5
neher-igel <neher@igel.com>
2022-10-13 19:53:36 -0600

    Linphone_VOIP (13 October)

---
+++ CP_Source/Unified_Communications/Linphone_VOIP/README.md
@@ -0,0 +1,11 @@
+# Linphone VOIP/SIP Client (13 October)
+
+|  CP Information |            |
+|--------------------|------------|
+| Package | [Linphone](https://linphone.org/) open source application using SIP on Linux |
+| Script Name | [linphone-cp-init-script.sh](build/linphone-cp-init-script.sh) |
+| CP Mount Path | /custom/linphone |
+| CP Size | 500M |
+| Build Notes | Details can be found in the build script [build-linphone-cp.sh](build/build-linphone-cp.sh) |
+| Packaging Notes | The CP Linphone is built from an AppImage. An AppImage will run on Linux without installation,  because everthing they need is built into that package. AppImages are like .ISO files and not changeable.<br /><br /> So why does this script extract the AppImage into a CP?<br /><br /> This AppImage checks for a newer version and then offers the user to download this update as an AppImage. But this is not possible on IGEL OS. To disable this, one line in a file has to be changed (usr/share/linphone/linphonerc-factory, see build script). |
+|Package automation | [build-linphone.sh](build-linphone.sh) |
---
+++ CP_Source/Unified_Communications/Linphone_VOIP/build/build-linphone-cp.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+#set -x
+#trap read debug
+
+# Creating an IGELOS CP for Linphone
+## Development machine (Ubuntu 18.04)
+
+#https://download.linphone.org/releases/linux/app/
+#Linphone-4.4.10.AppImage
+# Download latest AppImage
+#APPIMAGEVERSIONS=`wget -O - $BASEURL 2>/dev/null | sed -E 's/^.*href/href/; s/>.*//' | grep AppImage | awk -F"=" '{print $2}' | tr -d \" `
+
+if ! compgen -G "$HOME/Downloads/Linphone-*.AppImage" > /dev/null; then
+  echo "***********"
+  echo "Obtain latest package, save into $HOME/Downloads and re-run this script "
+  echo "https://download.linphone.org/releases/linux/app/"
+  exit 1
+fi
+
+sudo apt install unzip -y
+
+mkdir build_tar
+cd build_tar
+
+mkdir -p custom/linphone
+
+# Final package destination
+BASEDIR=$PWD/custom/linphone
+
+APPIMAGE0="$HOME/Downloads/Linphone-*.AppImage"
+APPIMAGE="$(basename $HOME/Downloads/Linphone-*.AppImage)"
+
+APPIMAGEVERSION=` echo $APPIMAGE  | cut -d- -f 2 | cut -d. -f 1-3`
+
+# chmod a+x
+chmod a+x $APPIMAGE0
+
+# executing AppImage
+# Next version?: change to mount -o loop ....
+$APPIMAGE0 2>&1 >/dev/null &
+
+# wait for Linphone to start
+while [ `ps ax | grep AppRun.wrapped | grep -v grep | wc -l` -lt 1 ]
+do
+    echo "."
+    sleep 1
+done
+
+echo "Linphone started, proceding.............."
+
+#where is it mounted under /tmp ?
+APPTEMPDIR=`mount | grep Lin | awk '{print $3}'`
+
+# copy it to the CP
+(cd $APPTEMPDIR;cp -r -p * $BASEDIR)
+
+# killall instances
+killall AppRun.wrapped
+
+# comment one line in usr/share/linphone/linphonerc-factory to disable download messages for the CP
+sed -i '/^version_check_url_root.*$/s/^/#/' ${BASEDIR}/usr/share/linphone/linphonerc-factory
+
+############################################
+# START: comment out for non-persistency!!!!
+############################################
+
+mkdir -p custom/linphone/userhome/.config/linphone
+mkdir -p custom/linphone/userhome/Documents/linphone/captures
+mkdir -p custom/linphone/userhome/.local/share/linphone/logs
+
+##########################################
+# END: comment out for non-persistency!!!!
+##########################################
+
+wget https://github.com/IGEL-Community/IGEL-Custom-Partitions/raw/master/CP_Packages/Unified_Communications/Linphone_VOIP.zip
+
+unzip Linphone_VOIP.zip -d custom
+mv custom/target/build/linephone-cp-init-script.sh custom
+
+cd custom
+
+# edit inf file for version number
+VERSION=${APPIMAGEVERSION}
+#echo "Version is: " ${VERSION}
+sed -i "/^version=/c version=\"${VERSION}\"" target/linephone.inf
+#echo "linephone.inf file is:"
+#cat target/linephone.inf
+
+# new build process into zip file
+tar cvjf target/linephone.tar.bz2 linephone linephone-cp-init-script.sh
+zip -g ../Linphone_VOIP.zip target/linephone.tar.bz2 target/linephone.inf
+zip -d ../Linphone_VOIP.zip "target/build/*" "target/igel/*" "target/target/*"
+mv ../Linphone_VOIP.zip ../../Linphone_VOIP-${VERSION}_igel01.zip
+
+cd ../..
+rm -rf build_tar
