b08ab108061175fba9353e4085e9ac124b386332
neher-igel <neher@igel.com>
2022-10-18 18:10:27 -0600

    Datto RMM Agent (18 October)

---
+++ CP_Source/Apps/Datto_RMM_Agent/build/datto-cp-init-script.sh
@@ -0,0 +1,55 @@
+#! /bin/bash
+#set -x
+#trap read debug
+
+ACTION="custompart-datto_${1}"
+
+# mount point path
+MP=$(get custom_partition.mountpoint)
+
+# custom partition path
+CP="${MP}/datto"
+
+# output to systemlog with ID amd tag
+LOGGER="logger -it ${ACTION}"
+
+echo "Starting" | $LOGGER
+
+case "$1" in
+init)
+  # Initial permissions
+  chown -R root:root "${CP}" | $LOGGER
+  # Linking files and folders on proper path
+  find ${CP} | while read LINE
+  do
+    DEST=$(echo -n "${LINE}" | sed -e "s|${CP}||g")
+    if [ ! -z "${DEST}" -a ! -e "${DEST}" ]; then
+      # Remove the last slash, if it is a dir
+      [ -d $LINE ] && DEST=$(echo "${DEST}" | sed -e "s/\/$//g") | $LOGGER
+      if [ ! -z "${DEST}" ]; then
+        ln -sv "${LINE}" "${DEST}" | $LOGGER
+      fi
+    fi
+  done
+
+  ldconfig
+  #Start service
+  sudo systemctl daemon-reload
+  sudo systemctl enable cag.service
+  sudo systemctl start cag.service
+
+;;
+stop)
+  # unlink linked files
+  find ${CP} | while read LINE
+  do
+    DEST=$(echo -n "${LINE}" | sed -e "s|${CP}||g")
+    unlink $DEST | $LOGGER
+  done
+
+;;
+esac
+
+echo "Finished" | $LOGGER
+
+exit 0
