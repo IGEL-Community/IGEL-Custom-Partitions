042731e9f704880c7f54a601ff363c89beb4a6ff
neher-igel <neher@igel.com>
2023-03-20 16:33:35 -0600

    LibreOffice - Updated for building for OS11 or OS12

--- CP_Source/Office/LibreOffice/README.md
+++ CP_Source/Office/LibreOffice/README.md
@@ -1,16 +1,38 @@
-# LibreOffice (8 February)
+# LibreOffice (20 March)
+
+-----
+
+**NOTE:** Builder works for OS11 (build with Ubuntu 18.04 - bionic) and OS12 (build with Ubuntu 20.04 - focal)
+
+-----
 
 |  CP Information |            |
-|--------------------|------------|
+|-----------------|------------|
 | Package | [LibreOffice](https://www.libreoffice.org) - Current Version <br /><br /> [Installing LibreOffice on Linux](https://wiki.documentfoundation.org/Documentation/Install/Linux) <br /><br /> [LibreOffice Help](https://help.libreoffice.org)|
-| CP Mount Path | /custom/libreoffice |
-| CP Size | 900M |
-| IGEL OS Version (min) | 11.05.133 |
-| Packaging Notes | See build script for details. |
-| Package automation | [build-libreoffice-cp.sh](build/build-libreoffice-cp.sh) <br /><br /> Tested with 7.5.X |
+| Script Name | [libreoffice-cp-init-script.sh](build/libreoffice-cp-init-script.sh) |
+| Icon name | /custom/libreoffice/usr/share/icons/hicolor/256x256/apps/libreoffice7.5-main.png |
+| Command | /custom/libreoffice/opt/libreoffice7.5/program/soffice |
+| Packaging Notes | Details can be found in the build script |
+| Package automation | [build-libreoffice-cp.sh](build/build-libreoffice-cp.sh) |
+
+-----
 
 **NOTES**
 
+- The **version number is embedded in path** (currently 7.5) and references to 7.5 will need to be modified if using a different version.
+
+-----
+
+**NOTES OS11**
+
 - For Help to open files in browser, you will need to enable Firefox browser, allow the browser to read files on the filesystem **Sessions > Firefox Browser > Firefox Browser Global > Security > Hide local filesystem (disable / false)**, and disable **system.security.apparmor**. A reboot is needed **before** the apparmor change is in effect.
 
-- The **version number is embedded in path** (currently 7.5) and references to 7.5 will need to be modified if using a different version.
+-----
+
+**NOTES OS12**
+
+- For certain features of the software - but not most - Java is required. Java is notably required for Base.
+
+- Jave can be installed with [Azul Zulu JDK FX](https://github.com/IGEL-Community/IGEL-Custom-Partitions/tree/master/CP_Source/Apps/Azul_JDKFX)
+
+- A browser will need to be installed for viewing help files.
--- CP_Source/Office/LibreOffice/build/build-libreoffice-cp.sh
+++ CP_Source/Office/LibreOffice/build/build-libreoffice-cp.sh
@@ -2,12 +2,35 @@
 #set -x
 #trap read debug
 
-# Creating an IGELOS CP for LibreOffice
-## Development machine (Ubuntu 18.04)
+# Creating an IGELOS CP
+## Development machine Ubuntu (OS11 = 18.04; OS12 = 20.04)
+CP="libreoffice"
+ZIP_LOC="https://github.com/IGEL-Community/IGEL-Custom-Partitions/raw/master/CP_Packages/Office"
+ZIP_FILE="LibreOffice"
+FIX_MIME="TRUE"
+CLEAN="FALSE"
+OS11_CLEAN="11.07.100"
+OS12_CLEAN="12.01.100"
+USERHOME_FOLDERS="TRUE"
+USERHOME_FOLDERS_DIRS="custom/libreoffice/userhome/.config/libreoffice custom/libreoffice/userhome/Documents"
+APPARMOR="TRUE"
+GETVERSION_FILE="../../LibreOffice_*/DEBS/libreoffice*-base_*.deb"
+MISSING_LIBS_OS11=""
+MISSING_LIBS_OS12=""
 
-# Obtain latest package and save into Downloads
-# Download Latest App for Linux (Debian)
-# https://www.libreoffice.org/download/download/?type=deb-x86_64
+VERSION_ID=$(grep "^VERSION_ID" /etc/os-release | cut -d "\"" -f 2)
+
+if [ "${VERSION_ID}" = "18.04" ]; then
+  MISSING_LIBS="${MISSING_LIBS_OS11}"
+  IGELOS_ID="OS11"
+elif [ "${VERSION_ID}" = "20.04" ]; then
+  MISSING_LIBS="${MISSING_LIBS_OS12}"
+  IGELOS_ID="OS12"
+else
+  echo "Not a valid Ubuntu OS release. OS11 needs 18.04 (bionic) and OS12 needs 20.04 (focal)."
+  exit 1
+fi
+
 if ! compgen -G "$HOME/Downloads/LibreOffice_*_Linux_x86-64_deb.tar.gz" > /dev/null; then
   echo "***********"
   echo "Obtain latest Linux (64-bit) (deb) package, save into $HOME/Downloads and re-run this script "
@@ -29,7 +52,11 @@ sudo apt install unzip -y
 mkdir build_tar
 cd build_tar
 
-mkdir -p custom/libreoffice
+for lib in $MISSING_LIBS; do
+  apt-get download $lib
+done
+
+mkdir -p custom/${CP}
 
 tar zxvf $HOME/Downloads/LibreOffice_*_Linux_x86-64_deb.tar.gz
 LIBREOFFICEDIR="LibreOffice_*_Linux_x86-64_deb/DEBS"
@@ -41,38 +68,63 @@ done
 tar zxvf $HOME/Downloads/LibreOffice_*_Linux_x86-64_deb_helppack_*.tar.gz
 dpkg -x LibreOffice_*_Linux_x86-64_deb_helppack_*/DEBS/lib*.deb custom/libreoffice
 
-mv custom/libreoffice/usr/share/applications/ custom/libreoffice/usr/share/applications.mime
-mkdir -p custom/libreoffice/userhome/.config/libreoffice
-mkdir -p custom/libreoffice/userhome/Documents
+if [ "${FIX_MIME}" = "TRUE" ]; then
+  mv custom/${CP}/usr/share/applications/ custom/${CP}/usr/share/applications.mime
+fi
 
-wget https://github.com/IGEL-Community/IGEL-Custom-Partitions/raw/master/CP_Packages/Office/LibreOffice.zip
+if [ "${USERHOME_FOLDERS}" = "TRUE" ]; then
+  for folder in $USERHOME_FOLDERS_DIRS; do
+    mkdir -p $folder
+  done
+fi
 
-unzip LibreOffice.zip -d custom
-mkdir -p custom/libreoffice/config/bin
-mkdir -p custom/libreoffice/lib/systemd/system
-mv custom/target/build/libreoffice_cp_apparmor_reload custom/libreoffice/config/bin
-mv custom/target/build/igel-libreoffice-cp-apparmor-reload.service custom/libreoffice/lib/systemd/system/
-mv custom/target/build/libreoffice-cp-init-script.sh custom
+if [ "${CLEAN}" = "TRUE" ]; then
+  echo "+++++++=======  STARTING CLEAN of USR =======+++++++"
+  wget https://raw.githubusercontent.com/IGEL-Community/IGEL-Custom-Partitions/master/utils/igelos_usr/clean_cp_usr_lib.sh
+  chmod a+x clean_cp_usr_lib.sh
+  wget https://raw.githubusercontent.com/IGEL-Community/IGEL-Custom-Partitions/master/utils/igelos_usr/clean_cp_usr_share.sh
+  chmod a+x clean_cp_usr_share.sh
+  if [ "${IGELOS_ID}" = "OS11" ]; then
+    ./clean_cp_usr_lib.sh ${OS11_CLEAN}_usr_lib.txt custom/${CP}/usr/lib
+    ./clean_cp_usr_share.sh ${OS11_CLEAN}_usr_share.txt custom/${CP}/usr/share
+  else
+    ./clean_cp_usr_lib.sh ${OS12_CLEAN}_usr_lib.txt custom/${CP}/usr/lib
+    ./clean_cp_usr_share.sh ${OS12_CLEAN}_usr_share.txt custom/${CP}/usr/share
+  fi
+  echo "+++++++=======  DONE CLEAN of USR =======+++++++"
+fi
+
+wget ${ZIP_LOC}/${ZIP_FILE}.zip
+
+unzip ${ZIP_FILE}.zip -d custom
+
+if [ "${APPARMOR}" = "TRUE" ]; then
+  mkdir -p custom/${CP}/config/bin
+  mkdir -p custom/${CP}/lib/systemd/system
+  mv custom/target/build/${CP}_cp_apparmor_reload custom/${CP}/config/bin
+  mv custom/target/build/igel-${CP}-cp-apparmor-reload.service custom/${CP}/lib/systemd/system/
+fi
+mv custom/target/build/${CP}-cp-init-script.sh custom
 
 cd custom
 
 # edit inf file for version number
 mkdir getversion
 cd getversion
-ar -x ../../LibreOffice_*/DEBS/libreoffice*-base_*.deb
-tar xf control.tar.*
+ar -x ${GETVERSION_FILE}
+tar xf control.tar.* ./control
 VERSION=$(grep Version control | cut -d " " -f 2)
 #echo "Version is: " ${VERSION}
 cd ..
-sed -i "/^version=/c version=\"${VERSION}\"" target/libreoffice.inf
-#echo "libreoffice.inf file is:"
-#cat target/libreoffice.inf
+sed -i "/^version=/c version=\"${VERSION}\"" target/${CP}.inf
+#echo "${CP}.inf file is:"
+#cat target/${CP}.inf
 
 # new build process into zip file
-tar cvjf target/libreoffice.tar.bz2 libreoffice libreoffice-cp-init-script.sh
-zip -g ../LibreOffice.zip target/libreoffice.tar.bz2 target/libreoffice.inf
-zip -d ../LibreOffice.zip "target/build/*" "target/igel/*" "target/target/*"
-mv ../LibreOffice.zip ../../LibreOffice-${VERSION}_igel01.zip
+tar cvjf target/${CP}.tar.bz2 ${CP} ${CP}-cp-init-script.sh
+zip -g ../${ZIP_FILE}.zip target/${CP}.tar.bz2 target/${CP}.inf
+zip -d ../${ZIP_FILE}.zip "target/build/*" "target/igel/*" "target/target/*"
+mv ../${ZIP_FILE}.zip ../../${ZIP_FILE}-${VERSION}_${IGELOS_ID}_igel01.zip
 
 cd ../..
 rm -rf build_tar
