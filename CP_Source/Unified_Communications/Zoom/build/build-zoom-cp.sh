4bf930e2bd5842ce88f0bb6b075af7aad714ea5c
neher-igel <neher@igel.com>
2023-03-20 14:37:42 -0600

    Zoom - Updated for building for OS11 or OS12

--- CP_Source/Unified_Communications/Zoom/build/build-zoom-cp.sh
+++ CP_Source/Unified_Communications/Zoom/build/build-zoom-cp.sh
@@ -2,63 +2,111 @@
 #set -x
 #trap read debug
 
-# Creating an IGELOS CP for Zoom
-## Development machine (Ubuntu 18.04)
+# Creating an IGELOS CP
+## Development machine Ubuntu (OS11 = 18.04; OS12 = 20.04)
+CP="zoom"
+ZIP_LOC="https://github.com/IGEL-Community/IGEL-Custom-Partitions/raw/master/CP_Packages/Unified_Communications"
+ZIP_FILE="Zoom"
+FIX_MIME="TRUE"
+CLEAN="FALSE"
+OS11_CLEAN="11.07.100"
+OS12_CLEAN="12.01.100"
+USERHOME_FOLDERS="TRUE"
+USERHOME_FOLDERS_DIRS="custom/zoom/userhome/.zoom custom/zoom/userhome/.config"
+APPARMOR="TRUE"
+GETVERSION_FILE="../../zoom_amd64.deb"
+MISSING_LIBS_OS11="libxcb-xtest0"
+MISSING_LIBS_OS12="libxcb-xtest0"
+
+VERSION_ID=$(grep "^VERSION_ID" /etc/os-release | cut -d "\"" -f 2)
+
+if [ "${VERSION_ID}" = "18.04" ]; then
+  MISSING_LIBS="${MISSING_LIBS_OS11}"
+  IGELOS_ID="OS11"
+elif [ "${VERSION_ID}" = "20.04" ]; then
+  MISSING_LIBS="${MISSING_LIBS_OS12}"
+  IGELOS_ID="OS12"
+else
+  echo "Not a valid Ubuntu OS release. OS11 needs 18.04 (bionic) and OS12 needs 20.04 (focal)."
+  exit 1
+fi
+
 sudo apt install unzip -y
 
 mkdir build_tar
 cd build_tar
 
 wget https://zoom.us/client/latest/zoom_amd64.deb
-apt-get download libxcb-xtest0
 
-mkdir -p custom/zoom
+for lib in $MISSING_LIBS; do
+  apt-get download $lib
+done
 
-dpkg -x zoom_amd64.deb custom/zoom
-dpkg -x libxcb* custom/zoom
+mkdir -p custom/${CP}
 
-mv custom/zoom/usr/share/applications/ custom/zoom/usr/share/applications.mime
+find . -name "*.deb" | while read LINE
+do
+  dpkg -x "${LINE}" custom/${CP}
+done
 
-############################################
-# START: comment out for non-persistency!!!!
-############################################
+if [ "${FIX_MIME}" = "TRUE" ]; then
+  mv custom/${CP}/usr/share/applications/ custom/${CP}/usr/share/applications.mime
+fi
 
-mkdir -p custom/zoom/userhome/.zoom
-mkdir -p custom/zoom/userhome/.config
-touch custom/zoom/userhome/.config/zoomus.conf
+if [ "${USERHOME_FOLDERS}" = "TRUE" ]; then
+  for folder in $USERHOME_FOLDERS_DIRS; do
+    mkdir -p $folder
+  done
+  touch custom/zoom/userhome/.config/zoomus.conf
+fi
 
-##########################################
-# END: comment out for non-persistency!!!!
-##########################################
+if [ "${CLEAN}" = "TRUE" ]; then
+  echo "+++++++=======  STARTING CLEAN of USR =======+++++++"
+  wget https://raw.githubusercontent.com/IGEL-Community/IGEL-Custom-Partitions/master/utils/igelos_usr/clean_cp_usr_lib.sh
+  chmod a+x clean_cp_usr_lib.sh
+  wget https://raw.githubusercontent.com/IGEL-Community/IGEL-Custom-Partitions/master/utils/igelos_usr/clean_cp_usr_share.sh
+  chmod a+x clean_cp_usr_share.sh
+  if [ "${IGELOS_ID}" = "OS11" ]; then
+    ./clean_cp_usr_lib.sh ${OS11_CLEAN}_usr_lib.txt custom/${CP}/usr/lib
+    ./clean_cp_usr_share.sh ${OS11_CLEAN}_usr_share.txt custom/${CP}/usr/share
+  else
+    ./clean_cp_usr_lib.sh ${OS12_CLEAN}_usr_lib.txt custom/${CP}/usr/lib
+    ./clean_cp_usr_share.sh ${OS12_CLEAN}_usr_share.txt custom/${CP}/usr/share
+  fi
+  echo "+++++++=======  DONE CLEAN of USR =======+++++++"
+fi
 
-wget https://github.com/IGEL-Community/IGEL-Custom-Partitions/raw/master/CP_Packages/Unified_Communications/Zoom.zip
+wget ${ZIP_LOC}/${ZIP_FILE}.zip
 
-unzip Zoom.zip -d custom
-mkdir -p custom/zoom/config/bin
-mkdir -p custom/zoom/lib/systemd/system
-mv custom/target/build/zoom_cp_apparmor_reload custom/zoom/config/bin
-mv custom/target/build/igel-zoom-cp-apparmor-reload.service custom/zoom/lib/systemd/system/
-mv custom/target/build/zoom-cp-init-script.sh custom
+unzip ${ZIP_FILE}.zip -d custom
+
+if [ "${APPARMOR}" = "TRUE" ]; then
+  mkdir -p custom/${CP}/config/bin
+  mkdir -p custom/${CP}/lib/systemd/system
+  mv custom/target/build/${CP}_cp_apparmor_reload custom/${CP}/config/bin
+  mv custom/target/build/igel-${CP}-cp-apparmor-reload.service custom/${CP}/lib/systemd/system/
+fi
+mv custom/target/build/${CP}-cp-init-script.sh custom
 
 cd custom
 
 # edit inf file for version number
 mkdir getversion
 cd getversion
-ar -x ../../zoom_amd64.deb
-tar xf control.tar.xz ./control
+ar -x ${GETVERSION_FILE}
+tar xf control.tar.* ./control
 VERSION=$(grep Version control | cut -d " " -f 2)
 #echo "Version is: " ${VERSION}
 cd ..
-sed -i "/^version=/c version=\"${VERSION}\"" target/zoom.inf
-#echo "zoom.inf file is:"
-#cat target/zoom.inf
+sed -i "/^version=/c version=\"${VERSION}\"" target/${CP}.inf
+#echo "${CP}.inf file is:"
+#cat target/${CP}.inf
 
 # new build process into zip file
-tar cvjf target/zoom.tar.bz2 zoom zoom-cp-init-script.sh
-zip -g ../Zoom.zip target/zoom.tar.bz2 target/zoom.inf
-zip -d ../Zoom.zip "target/build/*" "target/igel/*" "target/target/*"
-mv ../Zoom.zip ../../Zoom-${VERSION}_igel01.zip
+tar cvjf target/${CP}.tar.bz2 ${CP} ${CP}-cp-init-script.sh
+zip -g ../${ZIP_FILE}.zip target/${CP}.tar.bz2 target/${CP}.inf
+zip -d ../${ZIP_FILE}.zip "target/build/*" "target/igel/*" "target/target/*"
+mv ../${ZIP_FILE}.zip ../../${ZIP_FILE}-${VERSION}_${IGELOS_ID}_igel01.zip
 
 cd ../..
 rm -rf build_tar
