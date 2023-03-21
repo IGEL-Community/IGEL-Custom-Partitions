ff3a091416fcb927ab2d45e7bcb4af957214df62
neher-igel <neher@igel.com>
2023-03-21 09:59:55 -0600

    TeamViewer - Updated for building for OS11 or OS12

--- CP_Source/Apps/TeamViewer/README.md
+++ CP_Source/Apps/TeamViewer/README.md
@@ -1,16 +1,21 @@
-# TeamViewer (20 January)
+# TeamViewer (21 March)
+
+-----
+
+**NOTE:** Builder works for OS11 (build with Ubuntu 18.04 - bionic) and OS12 (build with Ubuntu 20.04 - focal)
+
+-----
 
 |  CP Information |            |
 |-----------------|------------|
 | Package | [TeamViewer](https://www.teamviewer.com/en-us/download/linux/) <br /><br /> - Establish incoming and outgoing connections between devices <br /> - Real-time remote access and support <br /><br /> The builder is able to build either: <br /><br /> - TeamViewer for Linux <br /> - TeamViewer Host|
 | Script Name | [teamviewer-cp-init-script.sh](build/teamviewer-cp-init-script.sh) |
-| CP Mount Path | /custom/teamviewer |
-| CP Size | 400M |
-| IGEL OS Version (min) | 11.05.133 |
-| Path to Executable | /custom/teamviewer/usr/bin/teamviewer |
-| Path to Icon | /custom/teamviewer/usr/share/icons/hicolor/256x256/apps/TeamViewer.png |
-| Packaging Notes | Details can be found in the build script [build-teamviewer-cp.sh](build/build-teamviewer-cp.sh) |
-| Package automation | [build-teamviewer-cp.sh](build/build-teamviewer-cp.sh) <br /><br /> This script will build the latest version based on Ubuntu 18.04 |
+| Icon name | /custom/teamviewer/usr/share/icons/hicolor/256x256/apps/TeamViewer.png |
+| Command | /custom/teamviewer/usr/bin/teamviewer |
+| Packaging Notes | Details can be found in the build script |
+| Package automation | [build-teamviewer-cp.sh](build/build-teamviewer-cp.sh) |
+
+-----
 
 **NOTE:** A reboot is required before the TeamViewer service can start.
 
--- CP_Source/Apps/TeamViewer/build/build-teamviewer-cp.sh
+++ CP_Source/Apps/TeamViewer/build/build-teamviewer-cp.sh
@@ -2,12 +2,35 @@
 #set -x
 #trap read debug
 
-# Creating an IGELOS CP for TeamViewer
-## Development machine (Ubuntu 18.04)
-# Obtain latest package and save into Downloads
-# Download Latest App for Linux (Debian)
-# https://www.teamviewer.com/en-us/download/linux/
-# teamviewer*_amd64.deb
+# Creating an IGELOS CP
+## Development machine Ubuntu (OS11 = 18.04; OS12 = 20.04)
+CP="teamviewer"
+ZIP_LOC="https://github.com/IGEL-Community/IGEL-Custom-Partitions/raw/master/CP_Packages/Apps"
+ZIP_FILE="TeamViewer"
+FIX_MIME="TRUE"
+CLEAN="FALSE"
+OS11_CLEAN="11.08.200"
+OS12_CLEAN="12.01.100"
+USERHOME_FOLDERS="TRUE"
+USERHOME_FOLDERS_DIRS="custom/teamviewer/userhome/.config/teamviewer"
+APPARMOR="FALSE"
+GETVERSION_FILE="../../${CP}*_amd64.deb"
+MISSING_LIBS_OS11=""
+MISSING_LIBS_OS12=""
+
+VERSION_ID=$(grep "^VERSION_ID" /etc/os-release | cut -d "\"" -f 2)
+
+if [ "${VERSION_ID}" = "18.04" ]; then
+  MISSING_LIBS="${MISSING_LIBS_OS11}"
+  IGELOS_ID="OS11"
+elif [ "${VERSION_ID}" = "20.04" ]; then
+  MISSING_LIBS="${MISSING_LIBS_OS12}"
+  IGELOS_ID="OS12"
+else
+  echo "Not a valid Ubuntu OS release. OS11 needs 18.04 (bionic) and OS12 needs 20.04 (focal)."
+  exit 1
+fi
+
 if ! compgen -G "$HOME/Downloads/teamviewer*_amd64.deb" > /dev/null; then
   echo "***********"
   echo "Obtain latest .deb package, save into $HOME/Downloads and re-run this script "
@@ -15,8 +38,6 @@ if ! compgen -G "$HOME/Downloads/teamviewer*_amd64.deb" > /dev/null; then
   echo "***********"
   exit 1
 fi
-
-MISSING_LIBS="libminizip1 libxcb-xinerama0"
 
 sudo apt install unzip -y
 
@@ -27,42 +48,72 @@ for lib in $MISSING_LIBS; do
   apt-get download $lib
 done
 
-mkdir -p custom/teamviewer
+cp $HOME/Downloads/teamviewer*_amd64.deb .
 
-dpkg -x $HOME/Downloads/teamviewer*_amd64.deb custom/teamviewer
+mkdir -p custom/${CP}
 
-find . -type f -name "*.deb" | while read LINE
+find . -name "*.deb" | while read LINE
 do
-  dpkg -x "${LINE}" custom/teamviewer
+  dpkg -x "${LINE}" custom/${CP}
 done
 
-mv custom/teamviewer/usr/share/applications/ custom/teamviewer/usr/share/applications.mime
-mkdir -p custom/teamviewer/userhome/.config/teamviewer
+if [ "${FIX_MIME}" = "TRUE" ]; then
+  mv custom/${CP}/usr/share/applications/ custom/${CP}/usr/share/applications.mime
+fi
 
-wget https://github.com/IGEL-Community/IGEL-Custom-Partitions/raw/master/CP_Packages/Apps/TeamViewer.zip
+if [ "${USERHOME_FOLDERS}" = "TRUE" ]; then
+  for folder in $USERHOME_FOLDERS_DIRS; do
+    mkdir -p $folder
+  done
+fi
 
-unzip TeamViewer.zip -d custom
-mv custom/target/build/teamviewer-cp-init-script.sh custom
+if [ "${CLEAN}" = "TRUE" ]; then
+  echo "+++++++=======  STARTING CLEAN of USR =======+++++++"
+  wget https://raw.githubusercontent.com/IGEL-Community/IGEL-Custom-Partitions/master/utils/igelos_usr/clean_cp_usr_lib.sh
+  chmod a+x clean_cp_usr_lib.sh
+  wget https://raw.githubusercontent.com/IGEL-Community/IGEL-Custom-Partitions/master/utils/igelos_usr/clean_cp_usr_share.sh
+  chmod a+x clean_cp_usr_share.sh
+  if [ "${IGELOS_ID}" = "OS11" ]; then
+    ./clean_cp_usr_lib.sh ${OS11_CLEAN}_usr_lib.txt custom/${CP}/usr/lib
+    ./clean_cp_usr_share.sh ${OS11_CLEAN}_usr_share.txt custom/${CP}/usr/share
+  else
+    ./clean_cp_usr_lib.sh ${OS12_CLEAN}_usr_lib.txt custom/${CP}/usr/lib
+    ./clean_cp_usr_share.sh ${OS12_CLEAN}_usr_share.txt custom/${CP}/usr/share
+  fi
+  echo "+++++++=======  DONE CLEAN of USR =======+++++++"
+fi
+
+wget ${ZIP_LOC}/${ZIP_FILE}.zip
+
+unzip ${ZIP_FILE}.zip -d custom
+
+if [ "${APPARMOR}" = "TRUE" ]; then
+  mkdir -p custom/${CP}/config/bin
+  mkdir -p custom/${CP}/lib/systemd/system
+  mv custom/target/build/${CP}_cp_apparmor_reload custom/${CP}/config/bin
+  mv custom/target/build/igel-${CP}-cp-apparmor-reload.service custom/${CP}/lib/systemd/system/
+fi
+mv custom/target/build/${CP}-cp-init-script.sh custom
 
 cd custom
 
 # edit inf file for version number
 mkdir getversion
 cd getversion
-ar -x $HOME/Downloads/teamviewer*_amd64.deb
+ar -x ${GETVERSION_FILE}
 tar xf control.tar.* ./control
 VERSION=$(grep Version control | cut -d " " -f 2)
 #echo "Version is: " ${VERSION}
 cd ..
-sed -i "/^version=/c version=\"${VERSION}\"" target/teamviewer.inf
-#echo "teamviewer.inf file is:"
-#cat target/teamviewer.inf
+sed -i "/^version=/c version=\"${VERSION}\"" target/${CP}.inf
+#echo "${CP}.inf file is:"
+#cat target/${CP}.inf
 
 # new build process into zip file
-tar cvjf target/teamviewer.tar.bz2 teamviewer teamviewer-cp-init-script.sh
-zip -g ../TeamViewer.zip target/teamviewer.tar.bz2 target/teamviewer.inf
-zip -d ../TeamViewer.zip "target/build/*" "target/igel/*" "target/target/*"
-mv ../TeamViewer.zip ../../TeamViewer-${VERSION}_igel01.zip
+tar cvjf target/${CP}.tar.bz2 ${CP} ${CP}-cp-init-script.sh
+zip -g ../${ZIP_FILE}.zip target/${CP}.tar.bz2 target/${CP}.inf
+zip -d ../${ZIP_FILE}.zip "target/build/*" "target/igel/*" "target/target/*"
+mv ../${ZIP_FILE}.zip ../../${ZIP_FILE}-${VERSION}_${IGELOS_ID}_igel01.zip
 
 cd ../..
 rm -rf build_tar
