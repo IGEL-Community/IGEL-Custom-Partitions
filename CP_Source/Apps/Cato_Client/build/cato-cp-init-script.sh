4bcbed82b5ac9db29b84a831115d317455d846b0
neher-igel <neher@igel.com>
2024-07-08 13:26:23 -0600

    Cato Client

---
+++ CP_Source/Apps/Cato_Client/build/cato-cp-init-script.sh
@@ -0,0 +1,57 @@
+#! /bin/bash
+#set -x
+#trap read debug
+
+ACTION="custompart-cato_${1}"
+
+# mount point path
+MP=$(get custom_partition.mountpoint)
+
+# custom partition path
+CP="${MP}/cato"
+
+# config directory
+#USER_CONFIG="/userhome"
+
+# output to systemlog with ID amd tag
+LOGGER="logger -it ${ACTION}"
+
+echo "Starting" | $LOGGER
+
+case "$1" in
+init)
+  # Initial permissions
+  chown -R root:root "${CP}" | $LOGGER
+  # Linking files and folders on proper path
+  find ${CP} -printf "/%P\n" | while read DEST
+  do
+    if [ ! -z "${DEST}" -a ! -e "${DEST}" ]; then
+      # Remove the last slash, if it is a dir
+      [ -d $DEST ] && DEST=${DEST%/} | $LOGGER
+      if [ ! -z "${DEST}" ]; then
+        ln -sv "${CP}/${DEST}" "${DEST}" | $LOGGER
+      fi
+    fi
+  done
+
+  # basic persistency
+  #if [ -d "${CP}${USER_CONFIG}" ]; then
+    #chown -R user:users "${CP}${USER_CONFIG}"
+  #fi
+
+;;
+stop)
+  # Unlinking files and folders on proper path
+  find ${CP} -printf "/%P\n" | while read DEST
+  do
+    if [ -L "${DEST}" ]; then
+      unlink $DEST | $LOGGER
+    fi
+  done
+
+;;
+esac
+
+echo "Finished" | $LOGGER
+
+exit 0
