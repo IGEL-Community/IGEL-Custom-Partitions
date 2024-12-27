a8ce311ef476fa44f82ecff4b3a1e8f9d25aefc3
neher-igel <neher@igel.com>
2024-12-27 11:05:53 -0700

    ringcentral citrix plugin ringcentral citrix plugin

---
+++ CP_Source/Unified_Communications/RingCentral_Phone_Citrix_Plugin/README.md
@@ -0,0 +1,8 @@
+# RingCentral Phone Citrix Plugin (27 December 2024)
+
+|  CP Information |            |
+|------------------|------------|
+| Package | [RingCentral Phone Citrix Plugin](https://support.ringcentral.com/article-v2/RingCentral-Phone-Citrix-Plugin-for-Windows.html?brand=RC_US&product=RingCentral_Phone&language=en_US) |
+| Script Name | [rcpcp-cp-init-script.sh](rcpcp-cp-init-script.sh) |
+| Packing Notes | See build script for details |
+| Package automation | [build-rcpcp-cp.sh](build/build-rcpcp-cp.sh) |
---
+++ CP_Source/Unified_Communications/RingCentral_Phone_Citrix_Plugin/build/rcpcp-cp-init-script.sh
@@ -0,0 +1,53 @@
+#! /bin/bash
+#set -x
+#trap read debug
+
+ACTION="custompart-rcpcp_${1}"
+
+# mount point path
+MP=$(get custom_partition.mountpoint)
+
+# custom partition path
+CP="${MP}/rcpcp"
+
+# output to systemlog with ID amd tag
+LOGGER="logger -it ${ACTION}"
+
+echo "Starting" | $LOGGER
+
+case "$1" in
+init)
+  # Initial permissions
+  chown -R root:root "${CP}" | $LOGGER
+  # Linking files and folders on proper path
+  find ${CP} | while read LINE
+  do
+    DEST=$(echo -n "${LINE}" | sed -e "s|${CP}||g")
+    if [ ! -z "${DEST}" -a ! -e "${DEST}" ]; then
+      # Remove the last slash, if it is a dir
+      [ -d $LINE ] && DEST=$(echo "${DEST}" | sed -e "s/\/$//g") | $LOGGER
+      if [ ! -z "${DEST}" ]; then
+        ln -sv "${LINE}" "${DEST}" | $LOGGER
+      fi
+    fi
+  done
+
+  export INSTALL_PREFIX=${MP}
+  . ${MP}/rcpcp_citrix.sh
+  citrix_reinstall
+
+;;
+stop)
+  # unlink linked files
+  find ${CP} | while read LINE
+  do
+    DEST=$(echo -n "${LINE}" | sed -e "s|${CP}||g")
+    unlink $DEST | $LOGGER
+  done
+
+;;
+esac
+
+echo "Finished" | $LOGGER
+
+exit 0
