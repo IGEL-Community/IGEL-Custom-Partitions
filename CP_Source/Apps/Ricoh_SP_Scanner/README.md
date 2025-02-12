9d7aebb88f3093b6b4c33be94072d03cd4293c2e
neher-igel <neher@igel.com>
2025-02-12 11:29:22 -0700

    ricoh sp scanner sane ricoh sp scanner sane

---
+++ CP_Source/Apps/Ricoh_SP_Scanner/README.md
@@ -0,0 +1,16 @@
+# Ricoh SP Scanner SANE (12 February)
+
+-----
+
+**NOTE:** Builder works for OS11 (build with Ubuntu 18.04 - bionic) and OS12 (build with Ubuntu 20.04 - focal)
+
+-----
+
+|  CP Information |            |
+|-----------------|------------|
+| Package | [Ricoh SP Scanner SANE](https://www.pfu.ricoh.com/global/scanners/fi/dl/ubuntu-sp-11xxn.html) |
+| Script Name | [ricoh-cp-init-script.sh](build/ricoh-cp-init-script.sh) |
+| Packaging Notes | Details can be found in the build script |
+| Package automation | [build-ricoh-cp.sh](build/build-ricoh-cp.sh) |
+
+-----
---
+++ CP_Source/Apps/Ricoh_SP_Scanner/build/ricohr-cp-init-script.sh
@@ -0,0 +1,51 @@
+#! /bin/bash
+#set -x
+#trap read debug
+
+ACTION="custompart-ricoh_${1}"
+
+# mount point path
+MP=$(get custom_partition.mountpoint)
+
+# custom partition path
+CP="${MP}/ricoh"
+
+# output to systemlog with ID amd tag
+LOGGER="logger -it ${ACTION}"
+
+echo "Starting" | $LOGGER
+
+case "$1" in
+init)
+  # Initial permissions
+  chown -R root:root "${CP}" | $LOGGER
+  # Linking files and folders on proper path
+  find ${CP} -printf "/%P\n" | while read DEST
+  do
+    if [ ! -z "${DEST}" -a ! -e "${DEST}" ]; then
+      # Remove the last slash, if it is a dir
+      [ -d $DEST ] && DEST=${DEST%/} | $LOGGER
+      if [ ! -z "${DEST}" ]; then
+        ln -sv "${CP}/${DEST}" "${DEST}" | $LOGGER
+      fi
+    fi
+  done
+
+  bash ${MP}/ricoh_igel_postinst.sh
+
+;;
+stop)
+  # Unlinking files and folders on proper path
+  find ${CP} -printf "/%P\n" | while read DEST
+  do
+    if [ -L "${DEST}" ]; then
+      unlink $DEST | $LOGGER
+    fi
+  done
+
+;;
+esac
+
+echo "Finished" | $LOGGER
+
+exit 0
