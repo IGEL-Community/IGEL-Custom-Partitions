26e3b0fa7b3736a14764606691119a56ab14aa7f
rneher-igel <neher@igel.com>
2025-11-19 15:30:31 -0700

    fujitsu sane fujitsu sane

---
+++ CP_Source/Apps/Fujitsu_Scanner/README.md
@@ -0,0 +1,14 @@
+# Fujitsu Scanner SANE (19 November)
+
+-----
+
+**NOTE:** Builder works for OS11 (build with Ubuntu 18.04 - bionic) and OS12 (build with Ubuntu 20.04 - focal)
+
+-----
+
+|  CP Information |            |
+|-----------------|------------|
+| Package | [Fujitsu Scanner SANE](https://www.pfu.ricoh.com/global/scanners/fi/dl/ubuntu-fi-8040.html) |
+| Script Name | [fujitsu-cp-init-script.sh](build/fujitsu-cp-init-script.sh) |
+| Packaging Notes | Details can be found in the build script |
+| Package automation | [build-fujitsu-cp.sh](build/build-fujitsu-cp.sh) |
---
+++ CP_Source/Apps/Fujitsu_Scanner/build/fujitsu-cp-init-script.sh
@@ -0,0 +1,51 @@
+#! /bin/bash
+#set -x
+#trap read debug
+
+ACTION="custompart-fujitsu_${1}"
+
+# mount point path
+MP=$(get custom_partition.mountpoint)
+
+# custom partition path
+CP="${MP}/fujitsu"
+
+# output to systemlog with ID amd tag
+LOGGER="logger -it ${ACTION}"
+
+echo "Starting" | $LOGGER
+
+case "$1" in
+init)
+  # Initial permissions
+  chown -R root:root "${CP}" | $LOGGER
+  # Linking files and folders on proper path
+  find ${CP} -printf "/%P\n" | while read DEST
+  do
+    if [ ! -z "${DEST}" -a ! -e "${DEST}" ]; then
+      # Remove the last slash, if it is a dir
+      [ -d $DEST ] && DEST=${DEST%/} | $LOGGER
+      if [ ! -z "${DEST}" ]; then
+        ln -sv "${CP}/${DEST}" "${DEST}" | $LOGGER
+      fi
+    fi
+  done
+
+  bash ${MP}/fujitsu_igel_postinst.sh
+
+;;
+stop)
+  # Unlinking files and folders on proper path
+  find ${CP} -printf "/%P\n" | while read DEST
+  do
+    if [ -L "${DEST}" ]; then
+      unlink $DEST | $LOGGER
+    fi
+  done
+
+;;
+esac
+
+echo "Finished" | $LOGGER
+
+exit 0
